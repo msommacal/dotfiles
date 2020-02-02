560a30aad8cdd50330c98bd284a6e6a3d80829e3
Mathias SOMMACAL <azerty.qwerty34@ymail.com>
2020-02-02 12:50:02 +0100

    Add firefox files

---
+++ firefox/user.js
@@ -0,0 +1,23 @@
+// Required to use userChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Show punycode (for safety reason)
+user_pref("network.IDN_show_punycode", true);
+
+// Enable auto-export of bookmarks.html
+user_pref("browser.bookmarks.autoExportHTML", true);
+
+// Enable spell checking for both input and textarea fields
+user_pref("layout.spellcheckDefault", 2);
+
+// Let the http:// in the URL
+user_pref("browser.urlbar.trimURLs", false);
+
+// Ignore links when tab key is pressed to change focus
+user_pref("accessibility.tabfocus", 3);
+
+// Disable Pocket
+user_pref("extensions.pocket.enabled", false);
+
+// Set the interval between two session backup
+user_pref("browser.sessionstore.interval", 120000);
---
+++ firefox/user_mobile.js
@@ -0,0 +1,14 @@
+// Show punycode (for safety reason)
+user_pref("network.IDN_show_punycode", true);
+
+// Enable spell checking for both input and textarea fields
+user_pref("layout.spellcheckDefault", 2);
+
+// Let the http:// in the URL
+user_pref("browser.urlbar.trimURLs", false);
+
+// Disable prefetch
+user_pref("network.prefetch-next", false);
+
+// Enable zoom on all webpages
+user_pref("browser.ui.zoom.force-user-scalable", true);
